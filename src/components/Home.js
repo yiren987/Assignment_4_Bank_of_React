1912c0173675a8fb31274c68aaf9a8ea8c9e1a27
timzhen38 <44072887+timzhen38@users.noreply.github.com>
2022-04-08 23:58:49 -0400

    debit and updated membersince

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,7 +18,7 @@ class Home extends Component {
           <br/>
           <Link to="">Credits (to be implemented in the Assignment)</Link>
           <br/>
-          <Link to="">Debits (to be implemented in the Assignment)</Link>
+          <Link to="debits">Debits</Link>
           
           <AccountBalance accountBalance={this.props.accountBalance}/>
         </div>
